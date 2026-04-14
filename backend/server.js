d94d097e04f846029a29be53452033424242963a
ishu11051-design <ishu11051@gmail.com>
2026-04-15 00:30:34 +0530

    Initial backend server setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error(err));
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
